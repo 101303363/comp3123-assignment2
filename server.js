1b6fb1212cd7c9171b469a8dd3c408213874a3d5
101303363 <113069165+101303363@users.noreply.github.com>
2022-11-17 21:58:54 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
-const express =require('express');
-const mongoose = require('mongoose');
-const PORT = process.env.PORT || 3030;
-const app = express();
-app.use(express.json());
+import express, { Router, json } from "express"
+const app = express()
+const routes = Router()
 
 
-app.get("/main",(req,res)=>{
-    res.send("Welcome to Main page")
-});
-app.listen(PORT,()=>{
-    console.log("http://localhost:3030/")
-});
+app.use(json())
+const SERVER_PORT =3003
+
+app.route("/")
+.get((req,res)=>{
+    res.send("Before Main")
+
+})
+
+app.route("/api")
+    .get((req,res)=>{
+        res.send("Employees Main Page")
+
+    })
+
+   
+
+   
+
+
+    
+
+
+
+app.listen(SERVER_PORT,()=>{
+    console.log("http://localhost:3003/")
+    console.log("Successfully created the server")
+})
+
